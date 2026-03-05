be56cc41e965042f4369bf075bc3b9757727c98c
Harnoor Lal <harlal902@gmail.com>
2026-03-04 22:09:43 -0800

    refactor(cli): deduplicate entry.metadata() call in find_latest_binary
    Closes #284

--- src/main.rs
+++ src/main.rs
@@ -441,19 +441,16 @@ fn find_latest_binary() -> Result<PathBuf, Error> {
                 continue;
             }
         }
+        let meta = entry
+            .metadata()
+            .map_err(io_context("read metadata", &path))?;
         #[cfg(unix)]
         {
             use std::os::unix::fs::PermissionsExt;
-            let meta = entry
-                .metadata()
-                .map_err(io_context("read metadata", &path))?;
             if meta.permissions().mode() & 0o111 == 0 {
                 continue; // not executable
             }
         }
-        let meta = entry
-            .metadata()
-            .map_err(io_context("read metadata", &path))?;
         let mtime = meta
             .modified()
             .map_err(io_context("read modified time", &path))?;
